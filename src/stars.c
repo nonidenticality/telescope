76597398040df152e510370da18d43df7897850a
nonid <nonidenticality@protonmail.com>
2024-07-17 05:27:05 -0600

    Renders stars

--- src/stars.c
+++ src/stars.c
@@ -2,20 +2,43 @@
 
 #include "stars.h"
 
-WINDOW * win = NULL;
+static WINDOW * win = NULL;
+
+static int STARTING_Y = 0;
+static int STARTING_X = 0;
+static int WIDTH;
+static int HEIGHT;
 
 void setup() {
     initscr();
+
+    WIDTH = COLS - 1;
+    HEIGHT = LINES - 1;
 
     raw();
     cbreak();
     noecho();
 
-    win = newwin(LINES - 1, COLS - 1, 1, 1);
+    win = newwin(HEIGHT, WIDTH, STARTING_X, STARTING_Y);
+}
+
+void render_star(int x, int y) {
+    if (!(((x * y) - (x ^ y)) % 17)) {
+        mvwprintw(win, y, x, "*");
+    }
+}
+
+void render_stars() {
+    for (int i = STARTING_Y + 1; i < HEIGHT - 1; i++) {
+        for (int j = STARTING_X + 1; j < WIDTH - 1; j++) {
+            render_star(j, i);
+        }
+    }
 }
 
 void render(View *view) {
     box(win, 0, 0);
+    render_stars();
     wrefresh(win);
 }
 
