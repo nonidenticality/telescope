0fced2e29414d504d305ef46b926ea57eeaba142
nonid <nonidenticality@protonmail.com>
2024-07-31 22:40:05 -0600

    Gradiates alien spawn rate

--- src/aliens.c
+++ src/aliens.c
@@ -1,21 +1,28 @@
 #include <stdlib.h>
-
 #include "aliens.h"
 #include "clock.h"
 
+enum AlienState{
+    INACTIVE,
+    ACTIVE,
+    VROOMING,
+};
 
+static const enum AlienState INITIAL_STATE = INACTIVE;
 static const char *SHIP_CHAR = "(O)";
 
+
 typedef struct {
+    enum AlienState state;
     unsigned int x, y,
                 vX, vY,
                 toX, toY;
 } Alien;
 
-Alien aliens[3] = {
-    { 0, 0, 0, 0, 15, 15},
-    { 0, 0, 0, 0, 5, 5},
-    { 0, 0, 0, 0, 10, 10},
+static Alien aliens[3] = {
+    { INITIAL_STATE, 0, 0, 0, 0, 15, 15},
+    { INITIAL_STATE, 0, 0, 0, 0, 5, 5},
+    { INITIAL_STATE, 0, 0, 0, 0, 10, 10},
 };
 
 unsigned int aliens_length = sizeof(aliens) / sizeof(Alien);
@@ -29,15 +36,51 @@ void render_alien(WINDOW *win, Alien *alien) {
     mvwprintw(win, alien->y, alien->x, "%s", SHIP_CHAR);
 }
 
+void render_vroom(WINDOW *win, Alien *alien) {
+    mvwprintw(win, alien->y, alien->x, "o");
+}
+
 void render_aliens(WINDOW* win) {
     for (unsigned int i = 0; i < aliens_length; i++) {
         Alien alien = aliens[i];
-        render_alien(win, &alien);
+        if (alien.state == ACTIVE) {
+            render_alien(win, &alien);
+        } else if (alien.state == VROOMING) {
+            render_vroom(win, &alien);
+        }
     }
 }
 
+void cycle_aliens() {
+    unsigned long current_nanoseconds = get_current_nanoseconds();
+    if (!(current_nanoseconds % 171)) {
+        for (unsigned int i = 0; i < aliens_length; i++) {
+            if (!((i + 1 + current_nanoseconds) % aliens_length)) {
+                continue;
+            }
+            if (aliens[i].state == ACTIVE) {
+                aliens[i].state = INACTIVE;
+                break;
+            }
+        }
+    }
+
+    if (!(current_nanoseconds % 1171)) {
+        for (unsigned int i = 0; i < aliens_length; i++) {
+            if (!((i + 1 + current_nanoseconds) % aliens_length)) {
+                continue;
+            }
+            if (aliens[i].state == INACTIVE) {
+                aliens[i].state = ACTIVE;
+                break;
+            }
+        }
+    }
+
+}
+
 void spawn_new_aliens() {
-    
+    cycle_aliens();
 }
 
 void new_interest_point(Alien *alien) {
